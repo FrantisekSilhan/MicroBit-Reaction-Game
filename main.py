1f53f62d903d991079925dab3ce3e856aa9b8343
František Šilhán <91247814+FrantisekSilhan@users.noreply.github.com>
2022-01-28 18:22:06 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,51 @@
+pins.touch_set_mode(TouchTarget.P1, TouchTargetMode.CAPACITIVE)
+pins.touch_set_mode(TouchTarget.P2, TouchTargetMode.CAPACITIVE)
+
+game_started = False
+P1Cheated = False
+P2Cheated = False
+def main():
+    global game_started, P1Cheated, P2Cheated
+    P1Cheated = False
+    P2Cheated = False
+    game_started = False
+    basic.clear_screen()
+    basic.pause(randint(30, 100) * 100)
+    game_started = True
+    if P1Cheated and P2Cheated:
+        show("C")
+    elif P1Cheated:
+        show("B")
+    elif P2Cheated:
+        show("A")
+    else:
+        basic.show_icon(IconNames.YES, 0)
+        music.play_tone(Note.C, 1500)
+control.in_background(main)
+
+def PressedP1():
+    global game_started, P1Cheated, P2Cheated
+    if game_started:
+        basic.show_number(1, 0)
+        new_game()
+    else:
+        P1Cheated = True
+input.on_pin_pressed(TouchPin.P1, PressedP1)
+
+def PressedP2():
+    global game_started, P1Cheated, P2Cheated
+    if game_started:
+        basic.show_number(2, 0)
+        new_game()
+    else:
+        P2Cheated = True
+input.on_pin_pressed(TouchPin.P2, PressedP2)
+
+def new_game():
+    basic.pause(3000)
+    main()
+
+def show(value):
+    basic.show_string(value, 0)
+    music.play_tone(Note.C, 1500)
+    new_game()
---
+++ main.ts
@@ -0,0 +1,58 @@
+pins.touchSetMode(TouchTarget.P1, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
+let game_started = false
+let P1Cheated = false
+let P2Cheated = false
+function main() {
+    
+    P1Cheated = false
+    P2Cheated = false
+    game_started = false
+    basic.clearScreen()
+    basic.pause(randint(30, 100) * 100)
+    game_started = true
+    if (P1Cheated && P2Cheated) {
+        show("C")
+    } else if (P1Cheated) {
+        show("B")
+    } else if (P2Cheated) {
+        show("A")
+    } else {
+        basic.showIcon(IconNames.Yes, 0)
+        music.playTone(Note.C, 1500)
+    }
+    
+}
+
+control.inBackground(main)
+input.onPinPressed(TouchPin.P1, function PressedP1() {
+    
+    if (game_started) {
+        basic.showNumber(1, 0)
+        new_game()
+    } else {
+        P1Cheated = true
+    }
+    
+})
+input.onPinPressed(TouchPin.P2, function PressedP2() {
+    
+    if (game_started) {
+        basic.showNumber(2, 0)
+        new_game()
+    } else {
+        P2Cheated = true
+    }
+    
+})
+function new_game() {
+    basic.pause(3000)
+    main()
+}
+
+function show(value: string) {
+    basic.showString(value, 0)
+    music.playTone(Note.C, 1500)
+    new_game()
+}
+
